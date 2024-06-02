e07596a7df450637a683b307c3b0345131e6b5ac
jasarevicedis <e.jasarevic03@gmail.com>
2024-06-02 08:52:27 +0200

    Add project files.

---
+++ AlarmApp/Jobs/AlarmJob.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Microsoft.Extensions.Logging;
+
+namespace AlarmApp.Jobs
+{
+    internal class AlarmJob : IJob
+    {
+        private readonly ILogger<AlarmJob> _logger;
+
+        public AlarmJob(ILogger<AlarmJob> logger) {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation($"[[ Alarm ]] --> Time: {DateTime.Now}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AlarmApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using AlarmApp.Jobs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Quartz;
+
+var builder = Host.CreateDefaultBuilder()
+    .ConfigureServices((cxt, services) =>
+    {
+        services.AddQuartz();
+        services.AddQuartzHostedService(opt =>
+        {
+            opt.WaitForJobsToComplete = true;
+        });
+    }).Build();
+
+var schedulerFactory = builder.Services.GetRequiredService<ISchedulerFactory>();
+var scheduler = await schedulerFactory.GetScheduler();
+
+// define the job and tie it to our HelloJob class
+var job = JobBuilder.Create<AlarmJob>()
+    .WithIdentity("Alarm", "group1")
+    .Build();
+
+// Trigger the job to run now, and then every 40 seconds
+var trigger = TriggerBuilder.Create()
+    .WithIdentity("alarmTrigger", "group1")
+    .StartNow()
+    .WithSimpleSchedule(x => x
+        .WithIntervalInSeconds(10)
+        .RepeatForever())
+    .Build();
+
+await scheduler.ScheduleJob(job, trigger);
+
+await builder.RunAsync();
